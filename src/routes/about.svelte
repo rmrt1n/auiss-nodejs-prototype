5769b768b4fe87ba7e9257439d2d1397005f33f4
Ryan Martin <51780559+rmrt1n@users.noreply.github.com>
2021-12-24 12:38:42 +0800

    added missing routes

---
+++ src/routes/about.svelte
@@ -0,0 +1,27 @@
+<h1 style="margin-bottom: 1.5rem">About us</h1>
+<p>
+  Asia Pacific University Indonesian Student Society is an international student society, formed
+  back in 2005. Initially we were created by our seniors to provide Indonesians student a platform
+  to celebrate our own Independence Day and obviously, to also strengthen fellow Indonesians in APU
+  bonds. We proudly stand as one of the biggest and most active society in our campus with our
+  varieties of event and activities.
+</p>
+
+<br />
+
+<p>
+  Not so similar with other common student societies out there, we decided to create and grow our
+  community in family-like environment, thus encourage members growth not only in organizational
+  aspect, but also in their own talent and interest. Currently we have 7 active clubs specializing
+  in various types of sport and art.
+</p>
+
+<br />
+
+<p>
+  Aside from our weekly clubs' activities, we also have our own annually event for both internal and
+  external. From charity to cultural show, we got them all. We envision to create event not only to
+  entertain, but also educating and benefiting the societies in various way. And that’s how we able
+  snatch the award of most active international community and the award of charity &amp community in
+  2018.
+</p>
---
+++ src/routes/kedai.svelte
@@ -0,0 +1,1 @@
+<h1>Page in progress...</h1>
